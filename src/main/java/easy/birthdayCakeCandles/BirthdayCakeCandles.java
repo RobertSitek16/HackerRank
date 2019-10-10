21482923409c9053770bcd8138956b5550b6823f
Robert Sitek <robert.sitek16@gmail.com>
2019-10-10 20:33:44 +0200

    New exercise

---
+++ src/main/java/easy/birthdayCakeCandles/BirthdayCakeCandles.java
@@ -0,0 +1,34 @@
+package easy.birthdayCakeCandles;
+
+import java.util.Scanner;
+
+public class BirthdayCakeCandles {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the size of the array");
+        String n = scanner.nextLine();
+        System.out.println("Enter the "+ n +"  numbers");
+        String[] array = scanner.nextLine().split(" ");
+        int size = Integer.parseInt(n);
+        int [] candleHeights = new int[size];
+        int sum = 0;
+        for (int i = 0; i < size; i++){
+            int items = Integer.parseInt(array[i]);
+            candleHeights[i] = items;
+        }
+        int theHighest = candleHeights[0];
+        for (int i = 0; i < size; i++){
+            if (theHighest < candleHeights[i]){
+                theHighest = candleHeights[i];
+            }
+        }
+        for (int i = 0; i < size; i++){
+            if (candleHeights[i] == theHighest){
+                sum++;
+            }
+        }
+        System.out.println("Sum of the highest number: " + sum);
+        System.out.println("The highest number: " + theHighest);
+    }
+}

eebeddf89a8fc0a518aebb12cbfa154d7a2d3827
RobertSitek16 <robert.sitek16@gmail.com>
2020-01-09 17:18:51 +0100

    practice

---
+++ src/main/java/practice/problemSolving/algorithms/search/RansomNote.java
@@ -0,0 +1,29 @@
+package practice.problemSolving.algorithms.search;
+
+import java.util.*;
+
+public class RansomNote {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int m = in.nextInt();
+        int n = in.nextInt();
+        HashMap<String, Integer> magazine = new HashMap<>();
+        for (int i = 0; i < m; i++) {
+            String word = in.next();
+            magazine.put(word, magazine.getOrDefault(word, 0) + 1);
+        }
+        HashMap<String, Integer> ransom = new HashMap<>();
+        for (int i = 0; i < n; i++) {
+            String word = in.next();
+            ransom.put(word, ransom.getOrDefault(word, 0) + 1);
+        }
+
+        Optional<Integer> res = ransom.entrySet().stream()
+                .map(e -> magazine.getOrDefault(e.getKey(), 0) - e.getValue())
+                .filter(e -> e < 0)
+                .findFirst();
+
+        System.out.println(res.isPresent() ? "No" : "Yes");
+    }
+}

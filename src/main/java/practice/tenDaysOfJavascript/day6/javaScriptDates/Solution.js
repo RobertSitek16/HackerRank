e5f3f0f0cbe52918de2eef7bff3cee9a1a2f2cdd
Robert Sitek <robert.sitek16@gmail.com>
2020-01-26 13:39:08 +0100

    Practice with JavaScript

---
+++ src/main/java/practice/tenDaysOfJavascript/day6/javaScriptDates/Solution.js
@@ -0,0 +1,63 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(string => {
+        return string.trim();
+    });
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+// The days of the week are: "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"
+function getDayName(dateString) {
+    var day;
+    switch(new Date(dateString).getDay()){
+        case 0:
+            day = "Sunday";
+            break;
+        case 1:
+            day = "Monday";
+            break;
+        case 2:
+            day = "Tuesday";
+            break;
+        case 3:
+            day = "Wednesday";
+            break;
+        case 4:
+            day = "Thursday";
+            break;
+        case 5:
+            day = "Friday";
+            break;
+        case 6:
+            day = "Saturday";
+    }
+
+    return day;
+}
+
+
+function main() {
+    const d = +(readLine());
+
+    for (let i = 0; i < d; i++) {
+        const date = readLine();
+
+        console.log(getDayName(date));
+    }
+}
